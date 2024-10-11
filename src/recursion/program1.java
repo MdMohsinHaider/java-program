719a491b6f2717fbcda601cb2b2fad08609bdefe
Mohsin <mdmohsinhaider6@gmail.com>
2024-10-11 10:11:54 +0530

    recursion program 1 and 2

---
+++ src/recursion/program1.java
@@ -0,0 +1,14 @@
+package src.recursion;
+
+public class program1 {
+    public static void main(String[] args) {
+        one2Hundred(1);
+    }
+
+    public static void one2Hundred(int n){
+        if (n<=100){
+            System.out.println(n);
+            one2Hundred(n+1);
+        }
+    }
+}
---
+++ src/recursion/program2.java
@@ -0,0 +1,15 @@
+package src.recursion;
+
+public class program2 {
+
+    public static void main(String[] args) {
+        hundred2One(100);
+    }
+
+    public static void hundred2One(int n){
+        if(n>=0){
+            System.out.println(n);
+            hundred2One(n-1);
+        }
+    }
+}

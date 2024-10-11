e0f348a10df101c15a27a4a9aa134cfc9fbfe197
Mohsin <mdmohsinhaider6@gmail.com>
2024-10-11 10:20:30 +0530

    String program 1

---
+++ src/string/Program1.java
@@ -0,0 +1,11 @@
+package src.string;
+
+public class Program1 {
+    public static void main(String[] args) {
+        // main method
+    }
+
+    public static void getString(){
+        // logic
+    }
+}

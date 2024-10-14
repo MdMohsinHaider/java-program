ed5ce5b771424fc6731532a94bc41936e6440060
Mohsin <mdmohsinhaider6@gmail.com>
2024-10-14 22:10:48 +0530

    src/SwitchCase/Program1.java

--- src/SwitchCase/Program1.java
+++ src/SwitchCase/Program1.java
@@ -1,12 +1,39 @@
 package src.SwitchCase;
 
+import lib.HelpCode;
+
+import java.util.Scanner;
+
 public class Program1 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        HelpCode in = new HelpCode();
+        System.out.print("Enter + - * / % : ");
+       char ch = sc.next().charAt(0);
+        cal(in.inputInt(),ch,in.inputInt());
 
     }
 
-    public static void cal(){
-
+    public static void cal(int first ,char opt, int sec){
+        switch (opt){
+            case '+':
+                System.out.println(first+sec);
+                break;
+            case '-':
+                System.out.println(first-sec);
+                break;
+            case '*':
+                System.out.println(first*sec);
+                break;
+            case '/':
+                System.out.println(first/sec);
+                break;
+            case '%':
+                System.out.println(first%sec);
+                break;
+            default:
+                System.out.println("wrong input");
+        }
     }
 
 }

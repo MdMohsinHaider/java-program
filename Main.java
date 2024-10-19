e3dd98d0a8bd837666cf988e7cd309f0ff31a301
Mohsin <mdmohsinhaider6@gmail.com>
2024-10-19 15:10:03 +0530

    Program

--- Main.java
+++ Main.java
@@ -1,5 +1,38 @@
+import lib.HelpCode;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello java Programmer");
+//       infiniteStrongNumber();
+//        infinitePalindrom();
+        infinitePrimeNumber();
     }
+
+    public static void infiniteStrongNumber(){
+        int count =0;
+        for (int i = 0; ; i++) {
+            count++;
+            if (HelpCode.isStrongNumber(i)){
+                System.out.println(i +" < : number : "+count);
+            }
+        }
+    }
+
+    public static void infinitePalindrom(){
+        for (int i = 0; ; i++) {
+            if (HelpCode.isPalindrome(i)){
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static void infinitePrimeNumber(){
+        for (int i = 0; ; i++) {
+            if (HelpCode.isPrime(i)){
+                System.out.println(i);
+            }
+        }
+    }
+
+
 }

3279c928e20358b4fb2937c5758ee8770522fad2
Mohsin <mdmohsinhaider6@gmail.com>
2024-10-20 17:22:43 +0530

    Programs

--- src/number/Program15.java
+++ src/number/Program15.java
@@ -2,7 +2,6 @@ package src.number;
 
 /*
 *
-*
 *       1^2+2^2+3^2+----------------+100^2.
  *
 * */
@@ -10,8 +9,18 @@ package src.number;
 
 public class Program15 {
     public static void main(String[] args) {
-        //
+        getSum(100);
     }
 
-    public static
+    public static void getSum(int range){
+        int num=0;
+        for (int i = 0; i <=range ; i++) {
+            num = num+getPower(i);
+        }
+        System.out.println(num);
+    }
+
+    public static int getPower(int num){
+        return num*num;
+    }
 }

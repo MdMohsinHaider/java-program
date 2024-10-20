331e8826693a597580a39147b36f3c56f504fe63
Mohsin <mdmohsinhaider6@gmail.com>
2024-10-20 16:51:32 +0530

    Programs

---
+++ src/number/Program15.java
@@ -0,0 +1,17 @@
+package src.number;
+
+/*
+*
+*
+*       1^2+2^2+3^2+----------------+100^2.
+ *
+* */
+
+
+public class Program15 {
+    public static void main(String[] args) {
+        //
+    }
+
+    public static
+}

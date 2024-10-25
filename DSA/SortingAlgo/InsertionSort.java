8a9156eac13097b7ce16cb8969a0e38025344830
Mohsin <mdmohsinhaider6@gmail.com>
2024-10-26 03:41:52 +0530

    Insertion sort Algo

--- DSA/SortingAlgo/InsertionSort.java
+++ DSA/SortingAlgo/InsertionSort.java
@@ -2,16 +2,23 @@ package DSA.SortingAlgo;
 
 public class InsertionSort {
     public static void main(String[] args) {
-        //
+        int[] array = {18,15,14,20,10,17,21};
+        getInsertionSort(array);
+
+        // print
+        for (int p : array){
+            System.out.println(p);
+        }
     }
 
     public static void getInsertionSort(int[] a){
         int n = a.length;
         for (int i = 1; i <n ; i++) {
             int key = a[i];
-            int j = i=1;
+            int j = i-1;
             while (j >= 0 && a[j]> key){
                 a[j+1] = a[j];
+                j--;
             }
             a[j+1] = key;
         }

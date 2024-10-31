84be503c6348e4b7c182802ece4d30d1781f7d16
Mohsin <mdmohsinhaider6@gmail.com>
2024-10-31 14:35:09 +0530

    Help code  Replacing Math.sqrt(x) with a manual square root calculation This approach works the same way as Math.sqrt(x) by comparing i * i directly with x. It checks up to the square root of x without actually computing the square root, thus eliminating the predefined method.

--- com/jspider/lib/HelpCode.java
+++ com/jspider/lib/HelpCode.java
@@ -98,7 +98,7 @@ public class HelpCode {
     // prime number
     public static boolean isPrime(int x) {
         if (x <= 1) return false;
-        for (int i = 2; i <= Math.sqrt(x); i++)
+        for (int i = 2; i*i <= x; i++)
             if (x % i == 0) return false;
         return true;
     }

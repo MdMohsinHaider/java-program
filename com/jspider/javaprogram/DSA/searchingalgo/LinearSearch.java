13c6e9b186088e59f77ec07923c648d27ccac0b2
Mohsin <mdmohsinhaider6@gmail.com>
2024-11-20 00:12:49 +0530

    DSA

---
+++ com/jspider/javaprogram/DSA/searchingalgo/LinearSearch.java
@@ -0,0 +1,12 @@
+package com.jspider.javaprogram.DSA.searchingalgo;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4,5,6,7,8,9,10,12,13,14,22,33,44,55,66,77,88,99};
+        linearSearch(array);
+    }
+
+    private static void linearSearch(int[] array){
+        // logic code
+    }
+}

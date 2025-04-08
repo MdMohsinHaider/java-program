8301d1ba8e214fea6abaaffb5b8a41870845677e
Mohsin <mdmohsinhaider6@gmail.com>
2025-04-08 12:36:25 +0530

    String

---
+++ com/jspider/javaprogram/src/string/part2/ReverseWordsInString.java
@@ -0,0 +1,34 @@
+package com.jspider.javaprogram.src.string.part2;
+
+/**
+ * Write a Java Program for the below requirements:
+ * i/p: mohan and sohan are here.
+ * o/p: here are sohan and mohan
+ */
+public class ReverseWordsInString {
+    public static void main(String[] args) {
+        String string = "mohan and sohan are here";
+        String rev = reverseWordsString(string);
+        System.out.println(rev);
+    }
+
+    private static String reverseWordsString(String string){
+
+        // Split input into words
+        String[] words = string.split(" ");
+
+        // Reverse the words using String concatenation
+        String output = "";
+        for (int i = words.length - 1; i >= 0; i--) {
+            output += words[i];
+            if (i != 0) {
+                output += " ";
+            }
+        }
+
+        // Print final result
+        System.out.println("Output: " + output);
+        return output;
+    }
+}
+

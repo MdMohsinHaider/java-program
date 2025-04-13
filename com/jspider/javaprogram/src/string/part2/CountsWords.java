d89f3eba1a19e098f6d935b50767b2a131738d7b
Mohsin <mdmohsinhaider6@gmail.com>
2025-04-13 12:26:53 +0530

    Count program

--- com/jspider/javaprogram/src/string/part2/CountsWords.java
+++ com/jspider/javaprogram/src/string/part2/CountsWords.java
@@ -2,7 +2,7 @@ package com.jspider.javaprogram.src.string.part2;
 
 public class CountsWords {
     public static void main(String[] args) {
-        String string = " Hello    My Name is Md Mohsin Haider";
+        String string = " Hello      My Name is Md Mohsin Haider";
         System.out.println(getWordsCount(string));
     }
 
@@ -10,12 +10,12 @@ public class CountsWords {
         String string1 = string.trim();
         int count= 0;
         for (int i = 0; i < string1.length(); i++) {
-            char currentC = string.toLowerCase().charAt(i);
-//            if (c==' ' && !(string.charAt(i) >= 'a' && string.charAt(i) <='z')) count++;
-            if (currentC==' '){
-                char nextC = string.charAt(i+1);
-                if (nextC != ' ')
+            char currentChar = string1.charAt(i); // extract current char
+            if (currentChar==' '){
+                char nextChar = string1.charAt(i+1); // extract next char from current char
+                if (nextChar != ' '){
                     count++;
+                }
             }
         }
         return count+1;

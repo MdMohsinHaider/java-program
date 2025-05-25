ef4b6b00b0c60d29caeec6f3efb88d2cc0e1e3d4
Mohsin <mdmohsinhaider6@gmail.com>
2025-05-25 08:43:48 +0530

    RearrangeArray

---
+++ com/jspider/javaprogram/src/array/RearrangeArray.java
@@ -0,0 +1,8 @@
+package com.jspider.javaprogram.src.array;
+
+public class RearrangeArray {
+    public static void main(String[] args) {
+        int[] arr = {-4, 8, 7, -6, -5, -1, 4, 3, 0, -9};
+    }
+    
+}
